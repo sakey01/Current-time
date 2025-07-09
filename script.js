1e27d3653cd0badb126d8a62251e812f95f16197
Sakey01 <sheikh36ahmed@gmail.com>
2025-07-09 14:19:03 +0100

    file change

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-  const time = document.querySelector('time');
-      const secondHand = document.querySelector(".second-hand");
-      const minuteHand = document.querySelector(".minute-hand");
-      const hourHand = document.querySelector(".hour-hand");
+const time = document.querySelector("time");
+const secondHand = document.querySelector(".second-hand");
+const minuteHand = document.querySelector(".minute-hand");
+const hourHand = document.querySelector(".hour-hand");
 
-      function setDate() {
-        const now = new Date();
+function setDate() {
+  const now = new Date();
 
-        const seconds = now.getSeconds().toString().padStart(2, '0');
-        const secondDegrees = (seconds / 60) * 360;
-        secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+  const seconds = now.getSeconds().toString().padStart(2, "0");
+  const secondDegrees = (seconds / 60) * 360;
+  secondHand.style.transform = `rotate(${secondDegrees}deg)`;
 
-        console.log(secondDegrees);
+  console.log(secondDegrees);
 
-        const minutes = now.getMinutes().toString().padStart(2, '0');
-        const minuteDegrees = (minutes / 60) * 360;
-        minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
+  const minutes = now.getMinutes().toString().padStart(2, "0");
+  const minuteDegrees = (minutes / 60) * 360;
+  minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
 
-        const hours = now.getHours().toString().padStart(2, '0');
-        const hourDegrees = (hours / 60) * 360;
-        hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+  const hours = now.getHours().toString().padStart(2, "0");
+  const hourDegrees = (hours / 60) * 360;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 
-        time.innerHTML = `${hours}:${minutes}:${seconds}`;
-      }
-      setInterval(setDate, 1000);
+  time.innerHTML = `${hours}:${minutes}:${seconds}`;
+}
+setInterval(setDate, 1000);
