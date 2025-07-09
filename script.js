9bcb4b1ba0a3879b3629058adfd3fc6eb4b5486c
Sakey01 <sheikh36ahmed@gmail.com>
2025-07-09 14:33:59 +0100

    calculation fix

--- script.js
+++ script.js
@@ -7,17 +7,17 @@ function setDate() {
   const now = new Date();
 
   const seconds = now.getSeconds().toString().padStart(2, "0");
-  const secondDegrees = (seconds / 60) * 360;
+  const secondDegrees = (seconds / 60) * 360 + 90;
   secondHand.style.transform = `rotate(${secondDegrees}deg)`;
 
   console.log(secondDegrees);
 
   const minutes = now.getMinutes().toString().padStart(2, "0");
-  const minuteDegrees = (minutes / 60) * 360;
+  const minuteDegrees = (minutes / 60) * 360 + (seconds / 60) * 6 + 90;
   minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
 
   const hours = now.getHours().toString().padStart(2, "0");
-  const hourDegrees = (hours / 60) * 360;
+  const hourDegrees = (hours / 12) * 360 + (minutes / 60) * 30 + 90;
   hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 
   time.innerHTML = `${hours}:${minutes}:${seconds}`;
